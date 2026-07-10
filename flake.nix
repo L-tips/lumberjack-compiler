5a23a3200d5dedcb43fc2ce77714ab1b9c23181b
Justin Beaurivage <code@beaurivage.io>
2026-07-10 19:34:20 -0400

    Add nix flake for reuse in other projects

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Model compiler for the Lumberjack tree ensemble accelerator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      perSystem = { pkgs, self', ... }: {
+        packages = {
+          lumberjack-compiler = pkgs.rustPlatform.buildRustPackage {
+            pname = "lumberjack-compiler";
+            version = "0.1.0";
+
+            src = ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            cargoBuildFlags = [
+              "--package"
+              "lumberjack-compiler"
+            ];
+          };
+
+          default = self'.packages.lumberjack-compiler;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            clippy
+          ];
+        };
+      };
+    };
+}
